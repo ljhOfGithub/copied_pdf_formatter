5bf2411de52725736fd13579e3d88283a066978f
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2021-12-13 10:22:49 +0800

    add regExp to match the '[num]'

--- remove_blank_character.js
+++ remove_blank_character.js
@@ -9,83 +9,81 @@
 // @grant        none
 // ==/UserScript==
 function blank(){
-    'use strict';
-   var button = document.createElement("button"); //创建一个按钮
-   var host = window.location.host;
-   button.textContent = "格式化"; //按钮内容
-   button.style.width = "109px"; //按钮宽度
-   button.style.height = "38px"; //按钮高度
-   button.style.align = "center"; //文本居中
-   button.style.color = "white"; //按钮文字颜色
-   button.style.background = "#e33e33"; //按钮底色
-   button.style.border = "1px solid #e33e33"; //边框属性
-   button.style.borderRadius = "4px"; //按钮四个角弧度
-   if( host == "fanyi.youdao.com" )
-   {
-   document.getElementsByClassName('fanyi__operations--left')[0].appendChild(button);
-   }
-   if( host == "translate.google.cn")
-   {
-   document.getElementsByClassName('hgbeOc')[0].appendChild(button);
-   }
-   if( host == "fanyi.baidu.com" )
-   {
-   document.getElementsByClassName('trans-operation clearfix')[0].appendChild(button);
-   }
-   function clickBotton(){
-       var txt = "";
-       var id = "";
-       var classname = "";
-       var host = window.location.host;
-       if( host == "fanyi.baidu.com" )
-       {
-           id = "baidu_translate_input";
-       }
-       if( host == "fanyi.youdao.com" )
-       {
-           id = "inputOriginal";
-       }
-       if( host == "translate.google.cn" || host == "translate.google.com" )
-       {
-           classname = "er8xn";
+     'use strict';
+    var button = document.createElement("button"); //创建一个按钮
+    var host = window.location.host;
+    button.textContent = "格式化"; //按钮内容
+    button.style.width = "109px"; //按钮宽度
+    button.style.height = "38px"; //按钮高度
+    button.style.align = "center"; //文本居中
+    button.style.color = "white"; //按钮文字颜色
+    button.style.background = "#e33e33"; //按钮底色
+    button.style.border = "1px solid #e33e33"; //边框属性
+    button.style.borderRadius = "4px"; //按钮四个角弧度
+    if( host == "fanyi.youdao.com" )
+    {
+    document.getElementsByClassName('fanyi__operations--left')[0].appendChild(button);
+    }
+    if( host == "translate.google.cn")
+    {
+    document.getElementsByClassName('hgbeOc')[0].appendChild(button);
+    }
+    if( host == "fanyi.baidu.com" )
+    {
+    document.getElementsByClassName('trans-operation clearfix')[0].appendChild(button);
+    }
+    function clickBotton(){
+        var txt = "";
+        var id = "";
+        var classname = "";
+        var host = window.location.host;
+        if( host == "fanyi.baidu.com" )
+        {
+            id = "baidu_translate_input";
+        }
+        if( host == "fanyi.youdao.com" )
+        {
+            id = "inputOriginal";
+        }
+        if( host == "translate.google.cn" || host == "translate.google.com" )
+        {
+            classname = "er8xn";
 
-       }
-       txt = document.getElementById(id).value || document.getElementsByClassName('er8xn')[0].value;
-       for (var i=0;i<txt.length;i++)//去掉论文的-（用于英文单词的跨行连接）和python源代码中的注释
-       {
-           if(txt.indexOf("")||txt.indexOf("#")||txt.indexOf("///")||txt.indexOf("//"))
-           {
-               //txt = txt.replace("\n"," ");
-               //txt = txt.replace(".",". \n");
-               //txt = txt.replace(" ","");
-               txt = txt.replace("","");
-               txt = txt.replace("#","");
-               if(txt.indexOf('///')==1){
-               txt = txt.replace("///","");
-               console.log(txt.indexOf('///'))
-               continue;
-               }//else if(txt.indexOf("//")){
-               txt = txt.replace("//","");
-               //}
+        }
+        txt = document.getElementById(id).value || document.getElementsByClassName('er8xn')[0].value;
+        txt = txt.replace(/\[[0-9]*\]/g, "");
+        for (var i=0;i<txt.length;i++)//去掉论文的-（用于英文单词的跨行连接）和python源代码中的注释
+        {
+            if(txt.indexOf("")||txt.indexOf("#")||txt.indexOf("///")||txt.indexOf("//"))
+            {
+                //txt = txt.replace("\n"," ");
+                //txt = txt.replace(".",". \n");
+                //txt = txt.replace(" ","");
+                txt = txt.replace("","");
+                txt = txt.replace("#","");
+                if(txt.indexOf('///')==1){
+                txt = txt.replace("///","");
+                continue;
+                }
+                txt = txt.replace("//","");
+                //txt.replace(/\[^[0-9]*\]/g, "");
+            }
 
-           }
-
-       }
-
-       txt = txt.replace("e.g.",'for example,');
-       txt = txt.replace("i.e.",'namely');
-       txt = txt.replace(/\n/g,' ');
-       txt = txt.replace(/\./g,".\n");
-       //txt = txt.replace("//g","-");
-       //txt = txt.replace(/\n\s/g,"\n");
-       let t=document.getElementById(id);
-       let evt = document.createEvent('HTMLEvents');
-       evt.initEvent('input',true,true);
-       t.value = txt;
-       t.dispatchEvent(evt);
-   }
-   button.addEventListener("click", clickBotton);
+        }
+        txt = txt.replace("e.g.",'for example,');
+        txt = txt.replace("i.e.",'namely');
+        txt = txt.replace(/\n/g,' ');
+        txt = txt.replace(/\./g,".\n");
+        //txt = txt.replace("//g","-");
+        //txt = txt.replace(/\n\s/g,"\n");
+        let t=document.getElementById(id);
+        let evt = document.createEvent('HTMLEvents');
+        evt.initEvent('input',true,true);
+        t.value = txt;
+        t.dispatchEvent(evt);
+    }
+    button.addEventListener("click", clickBotton);
 }
 (function() {
-   blank();
+    blank();
 })();
